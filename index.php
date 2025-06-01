08e955a78f381bacb2a863dc2970b4624231facb
Teo Fernández <81999491+TeoFernandez@users.noreply.github.com>
2025-05-31 23:29:33 -0300

    Update index.php
    documentacion

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
-//inicio de sesion
+//inicio de sesion 
 session_start();
 include("incluidos/header.php");
 include("incluidos/menu.php");
 include("incluidos/contenido.php");
 include("incluidos/footer.php");
+
+
 ?>
